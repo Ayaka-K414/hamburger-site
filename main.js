655f316b84415d590ea10f51efe7119cf1d26efa
ayakakobayashi <414ayaka@gmail.com>
2023-11-06 23:48:38 +0900

    一旦JSまで完成

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
 $(".c-menu").on("click",function(){
-    $(this).toggleClass("is-active");
-    $(".p-sidebar").toggleClass("is-active");
-    $(".c-menu--close").toggleClass("is-active");
-    $(".c-menubg").toggleClass("is-active");
-    $("body").toggleClass("is-active");
+    $(this).addClass("is-active");
+    $(".p-sidebar").addClass("is-active");
+    $(".c-menu--close").addClass("is-active");
+    $(".c-menubg").addClass("is-active");
+    $("body").addClass("is-active");
+});
+
+$(".c-menu--close").on("click",function(){
+    $(this).removeClass("is-active");
+    $(".p-sidebar").removeClass("is-active");
+    $(".c-menu").removeClass("is-active");
+    $(".c-menubg").removeClass("is-active");
+    $("body").removeClass("is-active");
 });
