604531409eb68b5bfdd76b4bdc295d60de07179e
ayakakobayashi <414ayaka@gmail.com>
2023-11-06 16:26:15 +0900

    メニューボタン実装中２

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 $(".c-menu").on("click",function(){
     $(this).toggleClass("is-active");
     $(".p-sidebar").toggleClass("is-active");
-    $(".c-menu--close")toggleClass("is-active")
+    $(".c-menu--close").toggleClass("is-active");
 });
